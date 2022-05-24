8486482c4c3e70ab39ed3fbf9ec61ec96d72c9c8
EmiliyaAndamova <EmiliyaAndamova@WPIPCM>
2022-05-24 18:10:26 +0400

    Add project files.

---
+++ Person class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220524_Homework
+{
+    class Person_class
+    {
+        public Person_class()
+        {
+
+        }
+        
+        public string Name;
+        public string Surname;
+        public int Age;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _220524_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person_class X = new Person_class();
+            
+                X.Name = "Miri";
+                X.Surname = "Miriyev";
+                X.Age = 20;
+
+            if (X.Age > 18)
+            {
+                Console.WriteLine("true");
+            }
+
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+        }
+    }
+}
